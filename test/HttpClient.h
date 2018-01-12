0c98361182f10dd7d70a60a0d502bc70c9edeb96
GELLEZ <fgellez@cegid.fr>
2018-01-12 13:48:57 +0100

    Add includes for linux socket

--- test/HttpClient.h
+++ test/HttpClient.h
@@ -5,19 +5,19 @@
 #ifdef WIN32
     # include <WinSock2.h>
 #else
-    //#include <sys/types.h>
-    //#include <sys/socket.h>
-    //#include <netinet/in.h>
-    //#include <arpa/inet.h>
-    //#include <unistd.h> /* close */
-    //#include <netdb.h> /* gethostbyname */
+    #include <sys/types.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <unistd.h> /* close */
+    #include <netdb.h> /* gethostbyname */
     //#define INVALID_SOCKET -1
-    //#define SOCKET_ERROR -1
+    #define SOCKET_ERROR -1
     #define closesocket(s) close(s)
     typedef int SOCKET;
     typedef struct sockaddr_in SOCKADDR_IN;
-    //typedef struct sockaddr SOCKADDR;
-    //typedef struct in_addr IN_ADDR;
+    typedef struct sockaddr SOCKADDR;
+    typedef struct in_addr IN_ADDR;
 #endif
 
 class HttpClient
