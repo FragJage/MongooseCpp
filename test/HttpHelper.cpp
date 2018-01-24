0a6a9a5292a2c335ac4e770dcc102fa92e819709
GELLEZ <fgellez@cegid.fr>
2018-01-24 13:32:03 +0100

    Debug unittests on Appveyor

--- test/HttpHelper.cpp
+++ test/HttpHelper.cpp
@@ -1,16 +1,21 @@
 #include "HttpHelper.h"
 
+#include <iostream>
+
 bool HttpHelper::WaitResponse(MongooseCpp::WebServer& server, HttpClient& client)
 {
+std::cout << "HWAIT..." << std::endl;
     int i, imax=25;
     for(i=0; i<imax; i++)
     {
+std::cout << i << "...";
         server.Poll(100);
         if(client.WaitingResponse(100)) break;
     }
 
     if(i==imax) return false;
 
-    client.ReceiveResponse();
+std::cout << "BEFORE RECV..." << std::endl;
+	client.ReceiveResponse();
     return true;
 }
